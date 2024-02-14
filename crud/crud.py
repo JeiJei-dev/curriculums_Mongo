4dac4ad882f3623753e2dc35e11f88f7db9e463b
Henry Navarro <henrymanuelnavarro@gmail.com>
2024-02-14 02:19:14 -0400

    [ADD] crud package: upload_file -> function

--- crud/crud.py
+++ crud/crud.py
@@ -1,3 +1,5 @@
+from json import loads
+
 def create(records):
     pass
 
@@ -11,4 +13,7 @@ def delete(records):
     pass
 
 def upload_file(records):
-    pass
+    file_name = "curriculums.json" # input("Enter the file name: ")
+    with open(file_name) as file:
+        res = loads(file.read())
+        records.insert_many(res)
