f339a0d2faff322a7e3a663ae8468bf2275d564b
Henry Navarro <henrymanuelnavarro@gmail.com>
2024-02-15 14:33:51 -0400

    [ADD] crud package: create and read -> function

--- crud/crud.py
+++ crud/crud.py
@@ -1,10 +1,20 @@
 from json import loads
 
 def create(records):
-    pass
+    vals = []
+    option = ""
+    while option.upper() != "EXIT":
+        option = input("Please type the new record ('Exit' for leave): ")
+        if option.upper() != "EXIT":
+            value = loads(option)
+            vals.append(value)
 
 def read(records):
-    pass
+    query = loads(input("Type your Query: "))
+    res = records.find(query)
+    
+    for value in res:
+        print(value)
 
 def update(records):
     pass
@@ -16,4 +26,4 @@ def upload_file(records):
     file_name = "curriculums.json" # input("Enter the file name: ")
     with open(file_name) as file:
         res = loads(file.read())
-        records.insert_many(res)
+        res = records.insert_many(res)
